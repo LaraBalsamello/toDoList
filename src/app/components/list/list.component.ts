68c6436014e2e5b2ee8071b547c5c957b9b93e8f
lara balsamello <lara.balsamello@gmail.com>
2019-09-27 15:25:31 -0300

    changes for responsive

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit, EventEmitter, Output, Self, ComponentRef } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Self, ComponentRef, Renderer } from '@angular/core';
 
 @Component({
-  selector: 'app-list.boxes',
+  selector: 'app-list.boxes.responsive-box',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
-  
+
   viewRef: ComponentRef<ListComponent>;
   title;
   descr;
+  decorate;
   @Output() removeList: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor(private render:Renderer) { }
 
   deleteList(){
     this.viewRef.destroy();
   }
 
-
+  decorateText(description, evt) {
+    this.decorate = description; 
+    this.render.setElementClass(evt.target.parentElement.parentElement,"decorate-text",true);
+  }
 }
