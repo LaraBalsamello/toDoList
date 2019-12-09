53162f18ce444c905eaf968f534b3135e3ba5a01
laraBalsamello <lara.balsamello@gmail.com>
2019-12-09 15:46:55 -0300

    list components modified and methods added

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output, Self, ComponentRef, Renderer } from '@angular/core';
+import { Component, EventEmitter, Output, ComponentRef, Renderer, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-list.boxes.responsive-box',
@@ -10,17 +10,36 @@ export class ListComponent {
   viewRef: ComponentRef<ListComponent>;
   title;
   descr;
+  doneTasks = [];
   decorate;
   @Output() removeList: EventEmitter<any> = new EventEmitter();
 
-  constructor(private render:Renderer) { }
+  constructor(private render:Renderer, private elem: ElementRef) { }
 
   deleteList(){
     this.viewRef.destroy();
   }
 
-  decorateText(description, evt) {
-    this.decorate = description; 
+  decorateText(evt, des) {
     this.render.setElementClass(evt.target.parentElement.parentElement,"decorate-text",true);
+    this.getAllDone(des);
+  }
+
+  getAllDone(des) {
+    this.doneTasks.push(des);
+    console.log(des, this.doneTasks, this.descr)
+    for (const i in this.doneTasks) {
+      if (this.doneTasks.hasOwnProperty(i)) {
+        const element = this.doneTasks[i];
+        if(element === this.descr[i]){
+          console.log('same');
+        }
+      }
+    }
+    if(this.doneTasks == this.descr) {
+      console.log('allDone'); 
+    } else {
+      console.log('no es el mismo array')
+    }
   }
 }
